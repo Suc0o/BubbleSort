28adbe5a22022b6f293a8f235330b526e46f7ec8
Vinícius Mancini Meneses de Miranda <89050874+Suc0o@users.noreply.github.com>
2023-04-05 16:12:05 -0300

    Update QuickSort.cpp

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -1,56 +1,41 @@
 #include <stdio.h>
-#include <stdlib.h> 
 
-int part(int vet[], int inicio, int fim){
+void quick(int vet[], int inicio, int fim) {
 	
-	int esq, dir, pivo, aux;
-	
-	esq = inicio + 1;
-	dir = fim;
-	pivo = vet[inicio];
-
-	
-	while(esq <= dir){
-		
-		while(esq <= dir && vet[esq] <= pivo){
-			esq++;
-		}
-		while(dir > inicio && vet[dir] > pivo){
-			dir--;
-		}
-		
-		if(esq < dir){
-			aux = vet[esq];
-			vet[esq] = vet[dir];
-			vet[dir] = aux;
-		}
-	}
-	vet[inicio] = vet[dir];
-	vet[dir] = pivo;
-	return dir;
-}
-
-void quick(int vet[], int inicio, int fim){
-	
-	int pivo;
-	
-	if(inicio < fim){
-	
-	pivo = part(vet, inicio, fim);
-    quick(vet,inicio, pivo - 1);
-    quick(vet, pivo + 1, fim);
+    if (inicio >= fim) {
+        return;
     }
+    
+    int pivo = vet[fim];
+    int i = inicio;
+    
+    for (int j = inicio; j < fim; j++) {
+        if (vet[j] <= pivo) {
+            int aux = vet[i];
+            vet[i] = vet[j];
+            vet[j] = aux;
+            i++;
+        }
+    }
+    
+    int aux = vet[i];
+    vet[i] = vet[fim];
+    vet[fim] = aux;
+    
+    quick(vet, inicio, i - 1);
+    quick(vet, i + 1, fim);
 }
 
-int main(){
+int main() {
 	
-    int	vet[] = {4,5,3,7,6,1,2,9,8,0};
-	
-	quick(vet, 0, 10);
-	
-	for(int i = 0; i < 10; i++){
-		printf("%d ", vet[i]);
-	}
-	
-	return 0;
+    int vet[] = {4, 1, 6, 2, 3, 7, 5, 9, 0, 8};
+    int tam = sizeof(vet) / sizeof(int);
+    
+    quick(vet, 0, tam - 1);
+    
+    for (int i = 0; i < tam; i++) {
+        printf("%d ", vet[i]);
+    }
+    
+    return 0;
 }
