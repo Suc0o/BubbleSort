1c8f7f4cae8311eb325b3e73d5825db5edb2ba51
Vinícius Mancini Meneses de Miranda <89050874+Suc0o@users.noreply.github.com>
2023-04-11 15:05:34 -0300

    Update InsertionSort.cpp

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -1,31 +1,29 @@
 #include <stdio.h>
 
-int main(){
-	
-	int tam;
-	
-	printf("Digite o tamanho da lista: ");
-	scanf("%d", &tam);
-	
-	int lista[tam];
-	
-	for(int i = 0; i < tam; i++){
-		printf("Digite o %d numero da lista: ", i+1);
-		scanf("%d", &lista[i]);
-	}
-	
-	for(int i = 1; i < tam; i++){
-		int p = lista[i];
-		int c = i - 1;
-		
-		while(i >= 0 && lista[c] > p){
-			lista[c + 1] = lista[c];
-			c -= 1;
-			lista[c + 1] = p;
+int main() {
+    int tam;
+    printf("Digite o tamanho da lista: ");
+    scanf("%d", &tam);
+
+    int lista[tam];
+    for (int i = 0; i < tam; i++) {
+        printf("Digite o %d numero da lista: ", i + 1);
+        scanf("%d", &lista[i]);
+    }
+
+    for (int i = 1; i < tam; i++) {
+        int c = i - 1;
+        int p = lista[i];
+        while (c >= 0 && lista[c] > p) {
+            lista[c + 1] = lista[c];
+            c--;
+        }
+        lista[c + 1] = p;
+    }
+
+    for (int i = 0; i < tam; i++) {
+        printf("O %d numero da lista e: %d\n", i + 1, lista[i]);
 		}
-	}
-	
-		for(int i = 0; i < tam; i++){
-		printf("O %d numero da lista e: %d\n", i+1, lista[i]);
-	}
+return 0;
 }
+		
